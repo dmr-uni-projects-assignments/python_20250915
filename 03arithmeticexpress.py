e1a94e5cb82ec0c58b40bc13011f9d8181ab6fc0
doomertheboomer <hackerdoomer@gmail.com>
2025-09-15 16:24:44 +0700

    done all

--- 03arithmeticexpress.py
+++ 03arithmeticexpress.py
@@ -4,3 +4,8 @@
 # Calculate the result of (first + second) * third.
 # Print the result.
 
+n1 = eval(input("Input num1: "))
+n2 = eval(input("Input num2: "))
+n3 = eval(input("Input num3: "))
+
+print("(n1 + n2) * n3 =", (n1 + n2) * n3)
--- 04numcmp.py
+++ 04numcmp.py
@@ -4,3 +4,9 @@
 # Print whether the first number is greater than the second (True or False).
 # Print whether the two numbers are equal (True or False).
 
+n1 = eval(input("Input num1: "))
+n2 = eval(input("Input num2: "))
+
+# no idea if i should use ifs for this, sorry!
+print("n1 > n2 =", n1 > n2)
+print("n1 == n2 =", n1 == n2)
